b7724483aa8c1456775768dc7ff04dba75635281
borhanuddinamin <borhanuddinamin@gmail.com>
2024-01-12 00:41:18 +0600

    home page assign

---
+++ Src/GS.App/GS.App/Components/Pages/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace GS.App.Components.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
